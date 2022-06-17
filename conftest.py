650f2e282d6b46ebc5d44ff9b2204f4038233b18
Danil <kokda@mail.ru>
2022-06-17 09:20:08 +0500

    add cookie

--- conftest.py
+++ conftest.py
@@ -5,11 +5,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as chrome_options
 import fake_useragent
 
+
 @pytest.fixture
 def get_chrome_options():
     user_agent = fake_useragent
     options = chrome_options()
-    options.add_argument('--enable-cookie-encryption')
     options.add_argument('--start-maximized')
     options.add_argument('--window-size=1280,720')
     options.add_argument(f"__user-agent={user_agent}")
@@ -32,3 +32,29 @@ def setup(request, get_webdriver):
     driver.get(url)
     yield driver  # в отличии от return возвращает объект-генератор
     # driver.quit()
+
+
+@pytest.fixture
+def cookie(get_webdriver):
+    # get_webdriver.add_cookie({"name": "__Secure-access-token",
+    #                           "value": "3.104122666.S0NmUPAcQVqh-HJKqY9Ieg.23.l8cMBQAAAABiqg4TC2gY8KN3ZWKrNzk4MjQ5MTE1ODIAgJCg.20220615145350.20220615185131.XoBcHy1VXBAKkW18tYyAbQVBJBvEaWHkKYJcJSJUQsk",
+    #                           "domain": ".ozon.ru", "path": "/", "size": "181"})
+    # get_webdriver.add_cookie(
+    #     {"name": "__Secure-user-id", "value": "104122666", "domain": ".ozon.ru", "path": "/", "size": "25"})
+    get_webdriver.add_cookie(
+        {"name": "__Secure-ab-group", "value": "23", "domain": ".ozon.ru", "path": "/", "size": "19"})
+    # get_webdriver.add_cookie({"name": "__Secure-refresh-token",
+    #                           "value": "3.104122666.S0NmUPAcQVqh-HJKqY9Ieg.23.l8cMBQAAAABiqg4TC2gY8KN3ZWKrNzk4MjQ5MTE1ODIAgJCg.20220615145350.20220615185131.e9OfTdBaxt1cUFUhSIAjUU5INrapezc5P-Kweep5Cys",
+    #                           "domain": ".ozon.ru", "path": "/", "size": "182"}) - мб куки пользователя
+    # get_webdriver.add_cookie(
+    #     {"name": "__Secure-ext_xcid", "value": "532d465b45ed9eb5f79ff542488ad647", "domain": ".ozon.ru", "path": "/",
+    #      "size": "49"})
+    # get_webdriver.add_cookie({"name": "isFromYandex", "value": "true", "domain": ".ozon.ru", "path": "/", "size": "16"})
+    # get_webdriver.add_cookie(
+    #     {"name": "xcid", "value": "532d465b45ed9eb5f79ff542488ad647", "domain": "www.ozon.ru", "path": "/",
+    #      "size": "36"})
+    # get_webdriver.add_cookie({"name": "AREA_ID", "value": "25949", "domain": "www.ozon.ru", "path": "/", "size": "12"})
+    get_webdriver.add_cookie({"name": "__cf_bm",
+                              "value": "_oYcBXdnqm5PwCRBCUYNheoFsHWguLypN0_yYRk_1oA-1655311854-0-AewQGEYoHaq4aANveQVfuIYlWyC9uEI0I7f/yUza3KT3BdutxmCe8WDMXZncHTQTWHOuaIO0H13Zx71QaMETI20=",
+                              "domain": ".ozon.ru", "path": "/", "size": "152"})
+    return get_webdriver
--- main.py
+++ main.py
@@ -1,58 +1,41 @@
 import time
-import pickle
 import pytest
 from selenium import webdriver
 from conftest import get_webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 @pytest.mark.usefixtures('setup')
 class TestHomepage:
-    def test_homepage(self, get_webdriver):
-        time.sleep(1)
-        get_webdriver.add_cookie({"name": "__Secure-access-token",
-                                  "value": "3.104122666.S0NmUPAcQVqh-HJKqY9Ieg.23.l8cMBQAAAABiqg4TC2gY8KN3ZWKrNzk4MjQ5MTE1ODIAgJCg.20220615145350.20220615185131.XoBcHy1VXBAKkW18tYyAbQVBJBvEaWHkKYJcJSJUQsk", "domain":".ozon.ru", "path":"/", "size":"181"})
-        get_webdriver.add_cookie(
-             {"name": "__Secure-user-id", "value": "104122666", "domain":".ozon.ru", "path":"/", "size":"25"})
-        get_webdriver.add_cookie({"name":"__Secure-ab-group", "value":"23", "domain":".ozon.ru", "path":"/", "size":"19"})
-        get_webdriver.add_cookie({"name":"__Secure-refresh-token", "value":"3.104122666.S0NmUPAcQVqh-HJKqY9Ieg.23.l8cMBQAAAABiqg4TC2gY8KN3ZWKrNzk4MjQ5MTE1ODIAgJCg.20220615145350.20220615185131.e9OfTdBaxt1cUFUhSIAjUU5INrapezc5P-Kweep5Cys", "domain":".ozon.ru", "path":"/", "size":"182"})
-        get_webdriver.add_cookie({"name":"__Secure-ext_xcid", "value":"532d465b45ed9eb5f79ff542488ad647", "domain":".ozon.ru", "path":"/", "size":"49"})
-        get_webdriver.add_cookie({"name":"isFromYandex", "value":"true", "domain":".ozon.ru", "path":"/", "size":"16"})
-        get_webdriver.add_cookie({"name":"xcid", "value":"532d465b45ed9eb5f79ff542488ad647", "domain":"www.ozon.ru", "path":"/", "size":"36"})
-        get_webdriver.add_cookie({"name":"AREA_ID", "value":"25949", "domain":"www.ozon.ru", "path":"/", "size":"12"})
-        get_webdriver.add_cookie({"name":"__cf_bm", "value":"_oYcBXdnqm5PwCRBCUYNheoFsHWguLypN0_yYRk_1oA-1655311854-0-AewQGEYoHaq4aANveQVfuIYlWyC9uEI0I7f/yUza3KT3BdutxmCe8WDMXZncHTQTWHOuaIO0H13Zx71QaMETI20=", "domain":".ozon.ru", "path":"/", "size":"152"})
-        time.sleep(8)
-        get_webdriver.find_element(By.XPATH, '//*[@class="ui-j9"]').click()
-        # time.sleep(10)
+    def test_homepage(self, cookie, get_webdriver):
 
-        get_webdriver.add_cookie({"name": "__Secure-access-token",
-                                  "value": "3.104122666.S0NmUPAcQVqh-HJKqY9Ieg.23.l8cMBQAAAABiqg4TC2gY8KN3ZWKrNzk4MjQ5MTE1ODIAgJCg.20220615145350.20220615185131.XoBcHy1VXBAKkW18tYyAbQVBJBvEaWHkKYJcJSJUQsk",
-                                  "domain": ".ozon.ru", "path": "/", "size": "181"})
-        get_webdriver.add_cookie(
-            {"name": "__Secure-user-id", "value": "104122666", "domain": ".ozon.ru", "path": "/", "size": "25"})
-        get_webdriver.add_cookie(
-            {"name": "__Secure-ab-group", "value": "23", "domain": ".ozon.ru", "path": "/", "size": "19"})
-        get_webdriver.add_cookie({"name": "__Secure-refresh-token",
-                                  "value": "3.104122666.S0NmUPAcQVqh-HJKqY9Ieg.23.l8cMBQAAAABiqg4TC2gY8KN3ZWKrNzk4MjQ5MTE1ODIAgJCg.20220615145350.20220615185131.e9OfTdBaxt1cUFUhSIAjUU5INrapezc5P-Kweep5Cys",
-                                  "domain": ".ozon.ru", "path": "/", "size": "182"})
-        get_webdriver.add_cookie(
-            {"name": "__Secure-ext_xcid", "value": "532d465b45ed9eb5f79ff542488ad647", "domain": ".ozon.ru",
-             "path": "/", "size": "49"})
-        get_webdriver.add_cookie(
-            {"name": "isFromYandex", "value": "true", "domain": ".ozon.ru", "path": "/", "size": "16"})
-        get_webdriver.add_cookie(
-            {"name": "xcid", "value": "532d465b45ed9eb5f79ff542488ad647", "domain": "www.ozon.ru", "path": "/",
-             "size": "36"})
-        get_webdriver.add_cookie(
-            {"name": "AREA_ID", "value": "25949", "domain": "www.ozon.ru", "path": "/", "size": "12"})
-        get_webdriver.add_cookie({"name": "__cf_bm",
-                                  "value": "_oYcBXdnqm5PwCRBCUYNheoFsHWguLypN0_yYRk_1oA-1655311854-0-AewQGEYoHaq4aANveQVfuIYlWyC9uEI0I7f/yUza3KT3BdutxmCe8WDMXZncHTQTWHOuaIO0H13Zx71QaMETI20=",
-                                  "domain": ".ozon.ru", "path": "/", "size": "152"})
+
+        hov = WebDriverWait(cookie, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@class="ui-c9 ui-d0 ui-h3"]')))
+        hov.click()
+        # hov = cookie.find_element(By.XPATH, '//*[@class="ui-c9 ui-d0 ui-h3"]').click()
+        # action = ActionChains(cookie)
+        # action.move_to_element(hov).perform()
+
+        time.sleep(3)
+        # iframe = cookie.find_element(By.XPATH, '//*[@class="vue-portal-target"]')
+        # cookie.switch_to.frame(iframe)
+
+        cookie.switch_to.frame(cookie.find_element(By.XPATH, '//*[@class="vue-portal-target"]'))
+
+
+        # get_webdriver.find_element(By.XPATH, '//input[@type="phone"]')
+
+
+        # WebDriverWait(cookie, 10).until(
+        # EC.presence_of_element_located((By.XPATH, '//*[@classd=class="bpa0"]')))
+ # ui-ca3
 
 
 
-        pickle.dump(get_webdriver.get_cookie("Name"), open("cookies1", "wb"))
-        print("Запись окончена")
 
 
 
